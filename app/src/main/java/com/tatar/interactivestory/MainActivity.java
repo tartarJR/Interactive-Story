97266690038acd9c216986e752b97159d5c6231b
Mustafa Ogün Öztürk <Mustafa Ogn ztrk>
2017-06-20 23:56:28 +0300

    EditText and Button activated

--- app/src/main/java/com/tatar/interactivestory/MainActivity.java
+++ app/src/main/java/com/tatar/interactivestory/MainActivity.java
@@ -1,13 +1,32 @@
 package com.tatar.interactivestory;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mNameEditText;
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNameEditText = (EditText) findViewById(R.id.nameEditText);
+        mStartButton = (Button) findViewById(R.id.startButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = mNameEditText.getText().toString();
+
+                Toast.makeText(MainActivity.this, "user name: " + name, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
